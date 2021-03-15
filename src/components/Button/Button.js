bd55d456034238dbaf9bc44f16503b0435b96110
samuelmarina <samuellmarina@gmail.com>
2021-03-14 20:44:52 -0400

    Create button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import colors from "../../config/colors/colors";
+
+const AppButton = ({children, color, disabled, onClick}) => {
+    const classes = useStyles();
+    return (
+        <Button
+            variant="outlined"
+            color={color}
+            disabled={disabled}
+            onClick={onClick}
+            className={classes.root}
+        >
+            {children}
+        </Button>
+    );
+};
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        paddingRight: "20px",
+        paddingLeft: "20px",
+        fontSize: "100%",
+        fontFamily: "Monda",
+        fontWeight: "bold",
+        color: colors.primary,
+        borderWidth: "2px",
+        borderColor: colors.secondary,
+        marginLeft: "15px",
+        width: '25%',
+        '&:hover': {
+            borderWidth: "2px",
+            borderColor: colors.buttonHover,
+            backgroundColor: "transparent"
+        },
+        '&:disabled': {
+            borderWidth: "2px",
+            borderColor: colors.buttonDisabled,
+            color: colors.buttonDisabled
+        },
+        [theme.breakpoints.down(1200)]: {
+            display: 'block',
+            width: '95%',
+            fontSize: '100%',
+            marginTop: '10px',
+        }
+    }
+}))
+
+export default AppButton;
